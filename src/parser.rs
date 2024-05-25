574ac114903ddf3cafb01110190fca33eadc6543
Felix Fong <fongf2@mcmaster.ca>
2024-05-25 16:17:20 -0400

    implemented parse_array

--- src/parser.rs
+++ src/parser.rs
@@ -19,61 +19,66 @@ pub enum Error {
     Unexpected(String),
 }
 
-fn parse_object(tokens: &[lexical::Token]) -> Result<Value, Vec<Error>> {
-    Ok(Value::Null)
+fn parse_object(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
+    Ok((Value::Null, &[]))
 }
 
-fn parse_array_elements(tokens: &[lexical::Token]) -> Result<Vec<Value>, Vec<Error>> {
-    let token = tokens;
-    // .next()
-    // .ok_or(vec![Error::Unexpected("End of file".to_string())])?;
+fn parse_array_elements(
+    tokens: &[lexical::Token],
+) -> Result<(Vec<Value>, &[lexical::Token]), Vec<Error>> {
+    if tokens.len() <= 1 {
+        return Err(vec![Error::Unexpected("End of file".to_string())]);
+    }
 
-    let mut elements = parse_array_elements(tokens);
-    Ok(Vec::new())
+    // match (tokens[0], tokens[1]) {
+    //     (token, lexical::Token::Punctuation(']')) => ,
+    // }
+    Ok((vec![], &[]))
 }
 
-fn parse_array(mut tokens: &[lexical::Token]) -> Result<Value, Vec<Error>> {
+fn parse_array(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
     let open_bracket = &tokens[0];
     assert!(*open_bracket == lexical::Token::Punctuation('['));
 
-    let token = &tokens[1];
-    // .peek()
-    // .ok_or(vec![Error::Unexpected("End of file".to_string())])?;
+    if tokens.len() == 1 {
+        return Err(vec![Error::Unexpected("End of file".to_string())]);
+    }
 
-    match token {
-        lexical::Token::Punctuation(']') => Ok(Value::Array(vec![])),
-        _ => Ok(Value::Array(parse_array_elements(
-            &tokens[1..tokens.len()],
-        )?)),
+    match &tokens[1] {
+        lexical::Token::Punctuation(']') => Ok((Value::Array(Vec::new()), &tokens[1..])),
+        _ => parse_array_elements(&tokens[1..])
+            .map(|(parsed_value, remaining_tokens)| (Value::Array(parsed_value), remaining_tokens)),
     }
 }
 
-fn parse_value(tokens: &[lexical::Token]) -> Result<Value, Vec<Error>> {
-    let token = &tokens[0];
-    // .peek()
-    // .ok_or(Err(vec![Error::Unexpected("End of file".to_string())]))?;
+fn parse_value(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
+    if tokens.len() == 0 {
+        return Err(vec![Error::Unexpected("End of file".to_string())]);
+    }
 
-    match token {
-        lexical::Token::Null => Ok(Value::Null),
-        lexical::Token::Bool(val) => Ok(Value::Bool(*val)),
-        lexical::Token::String(val) => Ok(Value::String((*val).as_str().to_string())),
-        lexical::Token::Number(val) => Ok(Value::Number(*val)),
+    let value = match &tokens[0] {
+        lexical::Token::Null => Value::Null,
+        lexical::Token::Bool(val) => Value::Bool(*val),
+        lexical::Token::String(val) => Value::String((*val).as_str().to_string()),
+        lexical::Token::Number(val) => Value::Number(*val),
         lexical::Token::Punctuation(c) => match *c {
-            '{' => Ok(parse_object(tokens)?),
-            '[' => Ok(parse_array(tokens)?),
-            ':' => Err(vec![Error::Expected("{".to_string())]),
-            ',' => Err(vec![Error::Unexpected(",".to_string())]),
-            '}' => Err(vec![Error::Expected("{".to_string())]),
-            ']' => Err(vec![Error::Expected("]".to_string())]),
+            '{' => return parse_object(tokens),
+            '[' => return parse_array(tokens),
+            ':' => Err(vec![Error::Expected("{".to_string())])?,
+            ',' => Err(vec![Error::Unexpected(",".to_string())])?,
+            '}' => Err(vec![Error::Expected("{".to_string())])?,
+            ']' => Err(vec![Error::Expected("]".to_string())])?,
             a => panic!("{a} is not a valid punctuation in JSON"),
         },
-    }
+    };
+
+    Ok((value, &tokens[1..]))
 }
 
 pub fn parse(json: &str) -> Result<Value, Vec<Error>> {
     let tokens = lexical::Token::try_from_json(json)
         .map_err(|x| x.into_iter().map(Error::LiteralError).collect::<Vec<_>>())?;
-    parse_value(&tokens[0..tokens.len()])
+    parse_value(&tokens[..]).map(|(parsed_value, _)| parsed_value)
 }
 
 #[cfg(test)]
