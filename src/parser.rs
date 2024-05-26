e6e15cc258b7396a79e59a60ca61e1a5578a5e89
Felix Fong <fongf2@mcmaster.ca>
2024-05-26 10:48:26 -0400

    refactored returns to be tuples of results of slices

--- src/parser.rs
+++ src/parser.rs
@@ -20,15 +20,18 @@ pub enum Error {
     MatchingOpeningPairNotFound(String),
 }
 
-fn parse_object(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
-    Ok((Value::Null, &[]))
+fn parse_object(_tokens: &[lexical::Token]) -> (Result<Value, Vec<Error>>, &[lexical::Token]) {
+    (Ok(Value::Null), &[])
 }
 
 fn parse_array_elements(
     tokens: &[lexical::Token],
-) -> Result<(Vec<Value>, &[lexical::Token]), Vec<Error>> {
+) -> (Result<Vec<Value>, Vec<Error>>, &[lexical::Token]) {
     if tokens.is_empty() {
-        return Err(vec![Error::UnexpectedEndOfFile("Expected ]".to_string())]);
+        return (
+            Err(vec![Error::UnexpectedEndOfFile("Expected ]".to_string())]),
+            &[],
+        );
     }
 
     let mut elements = Vec::<Value>::new();
@@ -39,14 +42,12 @@ fn parse_array_elements(
 
     loop {
         match parse_value(remaining_tokens) {
-            Ok((parsed_elements, next_remaining_tokens)) => {
+            (Ok(parsed_elements), next_remaining_tokens) => {
                 remaining_tokens = next_remaining_tokens;
                 elements.push(parsed_elements);
             }
-            Err(parse_errors) => {
-                if remaining_tokens[0] != lexical::Token::Punctuation(',') {
-                    remaining_tokens = &remaining_tokens[1..];
-                }
+            (Err(parse_errors), next_remaining_tokens) => {
+                remaining_tokens = next_remaining_tokens;
                 errors.extend(parse_errors);
             }
         }
@@ -80,65 +81,75 @@ fn parse_array_elements(
         assert!(
             remaining_tokens.is_empty() || remaining_tokens[0] == lexical::Token::Punctuation(',')
         );
-        Ok((elements, remaining_tokens))
+        (Ok(elements), remaining_tokens)
     } else {
-        Err(errors)
+        (Err(errors), remaining_tokens)
     }
 }
 
-fn parse_array(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
+fn parse_array(tokens: &[lexical::Token]) -> (Result<Value, Vec<Error>>, &[lexical::Token]) {
     let open_bracket = &tokens[0];
     assert!(*open_bracket == lexical::Token::Punctuation('['));
 
     if tokens.len() == 1 {
-        return Err(vec![Error::UnexpectedEndOfFile("Expected ]".to_string())]);
+        return (
+            Err(vec![Error::UnexpectedEndOfFile("Expected ]".to_string())]),
+            &[],
+        );
     }
 
     match tokens[1] {
-        lexical::Token::Punctuation(']') => Ok((Value::Array(Vec::new()), &tokens[2..])),
-        _ => parse_array_elements(&tokens[1..])
-            .map(|(parsed_value, remaining_tokens)| (Value::Array(parsed_value), remaining_tokens)),
+        lexical::Token::Punctuation(']') => (Ok(Value::Array(Vec::new())), &tokens[2..]),
+        _ => {
+            let (parsed_elements, remaining_tokens) = parse_array_elements(&tokens[1..]);
+            (parsed_elements.map(Value::Array), remaining_tokens)
+        }
     }
 }
 
-fn parse_value(tokens: &[lexical::Token]) -> Result<(Value, &[lexical::Token]), Vec<Error>> {
+fn parse_value(tokens: &[lexical::Token]) -> (Result<Value, Vec<Error>>, &[lexical::Token]) {
     if tokens.is_empty() {
-        return Err(vec![Error::UnexpectedEndOfFile(
-            "Expected value".to_string(),
-        )]);
+        return (
+            Err(vec![Error::UnexpectedEndOfFile(
+                "Expected value".to_string(),
+            )]),
+            &[],
+        );
     }
 
     let value = match &tokens[0] {
-        lexical::Token::Null => Value::Null,
-        lexical::Token::Bool(val) => Value::Bool(*val),
-        lexical::Token::String(val) => Value::String((*val).as_str().to_string()),
-        lexical::Token::Number(val) => Value::Number(*val),
+        lexical::Token::Null => Ok(Value::Null),
+        lexical::Token::Bool(val) => Ok(Value::Bool(*val)),
+        lexical::Token::String(val) => Ok(Value::String((*val).as_str().to_string())),
+        lexical::Token::Number(val) => Ok(Value::Number(*val)),
         lexical::Token::Punctuation(c) => match *c {
             '{' => return parse_object(tokens),
             '[' => return parse_array(tokens),
-            ':' => Err(vec![Error::Expected("{".to_string())])?,
-            ',' => Err(vec![Error::Expected("value".to_string())])?,
+            ':' => Err(vec![Error::Expected("{".to_string())]),
+            ',' => return (Err(vec![Error::Expected("value".to_string())]), tokens),
             '}' => Err(vec![Error::MatchingOpeningPairNotFound(
                 "{ not found".to_string(),
-            )])?,
+            )]),
             ']' => Err(vec![Error::MatchingOpeningPairNotFound(
                 "[ not found".to_string(),
-            )])?,
+            )]),
             a => panic!("{a} is not a valid punctuation in JSON"),
         },
     };
 
-    Ok((value, &tokens[1..]))
+    (value, &tokens[1..])
 }
 
 pub fn parse(json: &str) -> Result<Value, Vec<Error>> {
     let tokens = lexical::Token::try_from_json(json)
         .map_err(|x| x.into_iter().map(Error::LiteralError).collect::<Vec<_>>())?;
-    let (parsed_value, tokens) = parse_value(&tokens[..])?;
-    if !tokens.is_empty() {
-        Err(vec![Error::Expected("end of file".to_string())])
-    } else {
-        Ok(parsed_value)
+    match parse_value(&tokens[..]) {
+        (Err(mut errors), [_, ..]) => {
+            errors.push(Error::Expected("end of file".to_string()));
+            Err(errors)
+        }
+        (Ok(_), [_, ..]) => Err(vec![Error::Expected("end of file".to_string())]),
+        (parsed_json, []) => parsed_json,
     }
 }
 
@@ -191,11 +202,16 @@ mod tests {
     }
 
     #[test]
+    fn fail_many_commas() {
+        assert_eq!(Err(vec![]), parse(r#"[,,]"#));
+    }
+
+    #[test]
     fn fail_unclosed_array() {
         assert_eq!(
             Err(vec![Error::UnexpectedEndOfFile(
                 "Expected ',' or ']'".to_string()
-            )]),
+            ),]),
             parse("[true")
         );
     }
